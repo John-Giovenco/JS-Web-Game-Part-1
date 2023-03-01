a2bc49f32aecdbbb6f2b3af4a05ec7a6e1f5d3e4
John-Giovenco <JohnGiovenco93@gmail.com>
2023-02-28 20:22:33 -0800

    with corrected event listeners

--- index.js
+++ index.js
@@ -13,15 +13,14 @@ function newImage (url, left, bottom) {
     object.style.left = left + 'px'
     object.style.bottom = bottom + 'px'
     document.body.append(object)
-    return object
+    
+    object.addEventListener('click', function(){
+        object.remove()
+    })
 }
 
 function newItem(url, left, bottom) {
     let object = newImage(url, left, bottom)
-
-    object.addEventListener('dbclick', () => {
-        object.remove()
-    })
 }
 
 let horizon = window.innerHeight / 1.75
@@ -32,14 +31,14 @@ tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
 tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
 
 newImage('assets/green-character.gif', 100, 100)
-newImage('assets/tree', 200, 300)
+newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
 newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
 
 newItem('assets/sword.png', 500, 405)
-newItem('assets/shield.png', 165, 185)
+newItem('assets/sheild.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
 
 newItem('assets/grass.png',175, 385)
